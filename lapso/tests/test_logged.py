2d1df49361be76fa19cd83b06991291ffa43239c
abel gonzalez <odeceixe@gmail.com>
2019-08-27 02:04:10 +0200

    not need field _password_ in select, removing it

--- lapso/tests/test_logged.py
+++ lapso/tests/test_logged.py
@@ -25,7 +25,7 @@ def db(tmpdir):
 
 def test_logged_user_can_get_root_page_and_logout(app_client, db):
     row = db.execute(
-        "SELECT email, password FROM users"
+        "SELECT email FROM users"
     ).fetchone()
     user = {"email": row[0]}
     response = app_client.post(
