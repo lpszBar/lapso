a90d05923df5d69d5353eeb5c2cde2b980b8a41d
abel gonzalez <odeceixe@gmail.com>
2020-10-17 12:57:39 +0200

    removed react fiasco, moving back

--- init-app.sh
+++ init-app.sh
@@ -11,6 +11,6 @@ cd ..
 
 #cd static && (npm start &) && cd ..
 
-cd server
+cd lapso
 echo "Starting app in dir ${PWD}"
 FLASK_ENV=development FLASK_APP=app.py flask run --host="0.0.0.0" --port=5000
